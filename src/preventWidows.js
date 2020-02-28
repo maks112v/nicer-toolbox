a71f7ce2cfe2b70ddfa20a579b6589120155d4cb
Maksim V <maks112v@gmail.com>
2020-02-27 20:20:47 -0500

    issue fix

--- src/preventWidows.js
+++ src/preventWidows.js
@@ -1,7 +1,11 @@
 function preventWidows(str) {
-  if (!str) return null;
+  if (!str) {
+    return null;
+  }
   const words = str.trim().split(" ");
-  if (str.length < 3) return str;
+  if (str.length < 3) {
+    return str;
+  }
   const nbsp = "\u00a0";
   const lastWord = words.pop();
   return words.join(" ") + nbsp + lastWord;
