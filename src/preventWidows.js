ca7b50de0d25c2ef8c356b30db23369cf66ed078
Maksim V <maks112v@gmail.com>
2020-02-27 20:13:43 -0500

    simple version

--- src/preventWidows.js
+++ src/preventWidows.js
@@ -1,6 +1,7 @@
 function preventWidows(str) {
+  if (!str) return null;
   const words = str.trim().split(" ");
-  if (words.length < 3) return str;
+  if (str.length < 3) return str;
   const nbsp = "\u00a0";
   const lastWord = words.pop();
   return words.join(" ") + nbsp + lastWord;
